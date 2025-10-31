dfab7cd2409b3d5350123898936b79b18d9dc4c0
Rodrigue TOKO <dimagotoko@gmail.com>
2025-10-31 23:58:59 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -30,12 +30,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This is to allow the Next.js dev server to be accessed from the cloud workstation
-    allowedDevOrigins: [
-      'https://*.cloudworkstations.dev',
-    ],
-  },
+  // This is to allow the Next.js dev server to be accessed from the cloud workstation
+  allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
