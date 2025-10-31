8e08ec17f511fd3c31091cc86eb7ff74c373024d
Rodrigue TOKO <dimagotoko@gmail.com>
2025-10-31 23:06:06 +0000

    Try fixing this error: `Console Error: 💡 use-places-autocomplete: Googl

--- src/components/GoogleMapsProvider.tsx
+++ src/components/GoogleMapsProvider.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import { useLoadScript } from '@react-google-maps/api';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export function GoogleMapsProvider({ children }: { children: React.ReactNode }) {
   const { isLoaded, loadError } = useLoadScript({
@@ -14,8 +15,23 @@ export function GoogleMapsProvider({ children }: { children: React.ReactNode }) 
     return <div>Erreur de chargement de la carte. Avez-vous configuré la variable d'environnement NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?</div>;
   }
 
+  if (!isLoaded) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <div className="flex-1 p-4 md:p-8 lg:p-12">
+                <div className="space-y-4">
+                    <Skeleton className="h-12 w-1/2" />
+                    <Skeleton className="h-8 w-1/4" />
+                    <div className="grid gap-4 md:grid-cols-2">
+                        <Skeleton className="h-20" />
+                        <Skeleton className="h-20" />
+                    </div>
+                     <Skeleton className="h-20 w-full" />
+                </div>
+            </div>
+        </div>
+    );
+  }
+
   return <>{children}</>;
 }
-
-// We need to add @react-google-maps/api to dependencies
-// Let's modify package.json to add "@react-google-maps/api": "^2.19.3"
