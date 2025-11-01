5089896019a5fbc3b980ce96304539935b8c905c
Rodrigue TOKO <dimagotoko@gmail.com>
2025-11-01 01:29:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2194514521-a4a53",
+  "appId": "1:761287999801:web:da2713b9e85dd1040aa8e8",
+  "apiKey": "AIzaSyAJcrcgJVKHpVQkI9C2Qyo711-q8x1RO1c",
+  "authDomain": "studio-2194514521-a4a53.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "761287999801"
+};
