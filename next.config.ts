370a326c1534894f5fd4c829174622e8d50b55ec
Rodrigue TOKO <dimagotoko@gmail.com>
2025-11-01 01:12:19 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://9003-firebase-studio-1761943966842.cluster-2xid2zxbenc4ixa74rpk7q7fyk.cloudworkstations.dev',
+    ],
+  },
   images: {
     remotePatterns: [
       {
